c79cce96128c061e068995704ab486905c67f7a2
Sergio Linero <SLineroDev@gmail.com>
2021-10-25 08:20:35 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState, useEffect } from 'react'
+import { getJSONCharacters } from './services/api'
 import './App.css'
+import { Character } from './models/Character'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App (): JSX.Element {
+  const [characterList, setCharacterList] = useState<Character[]>([])
+
+  useEffect(() => {
+    getJSONCharacters()
+      .then(resp => { setCharacterList(resp.results) })
+      .catch((err) => console.log(err))
+  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <nav>
+      <ul>
+        {
+          (characterList != null)
+            ? characterList.map((character, index) => <li key={index}>{JSON.stringify(character)}</li>)
+            : <h3>No data</h3>
+        }
+      </ul>
+    </nav>
   )
 }
 
---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+import { CharacterResp } from '../models/Character'
+
+const API_URL = 'https://rickandmortyapi.com/api/character'
+
+export async function getAllCharacters (): Promise<CharacterResp> {
+  return await fetch(API_URL).then(async res => await res.json())
+}
+
+export async function getJSONCharacters (): Promise<CharacterResp> {
+  return await fetch('/data.json').then(async res => await res.json())
+}
