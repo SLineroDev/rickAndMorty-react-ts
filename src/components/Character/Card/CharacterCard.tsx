deb56f51fa01c76dc2b815de679dd627d1b8739d
Sergio Linero <SLineroDev@gmail.com>
2021-11-01 19:17:15 +0100

    Added placeholder for lazyload

--- src/components/Character/Card/CharacterCard.tsx
+++ src/components/Character/Card/CharacterCard.tsx
@@ -1,6 +1,7 @@
 import { Character } from 'models/Character'
 import LazyLoad from 'react-lazyload'
 import './CharacterCard.css'
+import CharacterCardPlaceholder from './CharacterCardPlaceholder'
 
 interface CharacterCardProps {
   character: Character
@@ -11,8 +12,8 @@ export default function CharacterCard (props: CharacterCardProps): JSX.Element {
 
   return (
     <div className='character-card'>
-      <LazyLoad offset={200} once>
-        <img src={image} alt='' />
+      <LazyLoad height={100} offset={100} once placeholder={<CharacterCardPlaceholder />}>
+        <img src={image} width='800px' height='800px' alt='' />
       </LazyLoad>
       <div><strong>{name}</strong></div>
       <div>{status}</div>
---
+++ src/components/Character/Card/CharacterCardPlaceholder.tsx
@@ -0,0 +1,6 @@
+
+export default function CharacterCardPlaceholder (): JSX.Element {
+  return (
+    <div style={{ height: '100px', backgroundColor: 'lightgrey' }} />
+  )
+}
